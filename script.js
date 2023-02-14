9b7b4b14c39f401aad438ee599964878de2adf9b
Ganesh Udutha <101318513+Ganesh486@users.noreply.github.com>
2023-02-14 15:45:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    $("name").text(profile.getName());
-    $("email").text(profile.getEmail());
+    $("#name").text(profile.getName());
+    $("#email").text(profile.getEmail());
     $("#image").attr('src',profile.getImageUrl());
     $(".data").css("display","block");
     $(".g-signin2").css("display","none");    
